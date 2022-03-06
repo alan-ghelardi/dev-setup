f566d71c6222e1431477290c8341c9dfeb2311a6
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2022-03-06 12:48:22 -0300

    Update kustomize.

--- dev-tools/kustomize.sh
+++ dev-tools/kustomize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-KUSTOMIZE_VERSION=4.0.1
+KUSTOMIZE_VERSION=4.5.2
 
 curl --silent --location https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz --output kustomize.tar.gz && \
     tar -xzvf kustomize.tar.gz && \
