25076e5eae9108ebcc87f8950c1fdec5e52d5ae6
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2022-06-28 17:13:37 -0300

    Upgrade Golang.

--- dev-tools/golang.sh
+++ dev-tools/golang.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-GOLANG_VERSION=1.16
+GOLANG_VERSION=1.17
 
 curl --silent --location https://golang.org/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz --output golang.tar.gz && \
     sudo tar -C /usr/local -xvzf golang.tar.gz && \
