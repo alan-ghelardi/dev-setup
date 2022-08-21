b6e8b27bf9577e73976e2654224181a7164a5204
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2022-08-21 10:36:20 -0300

    Install tkn results plugin.

--- dev-tools/tkn.sh
+++ dev-tools/tkn.sh
@@ -6,3 +6,6 @@ export TKN_VERSION=0.25.0
 curl -LO https://github.com/tektoncd/cli/releases/download/v${TKN_VERSION}/tkn_${TKN_VERSION}_Linux_x86_64.tar.gz
 sudo tar xvzf tkn_${TKN_VERSION}_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
 rm tkn_${TKN_VERSION}_Linux_x86_64.tar.gz
+
+# Install the Results plugin
+GOBIN=${TKN_PLUGINS_DIR:-"${HOME}/.config/tkn/plugins"} go install github.com/tektoncd/results/tools/tkn-results@latest
