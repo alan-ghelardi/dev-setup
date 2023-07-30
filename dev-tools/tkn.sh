f4dea3904d0ad913a04164c09f8e7d88b9a48554
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2023-07-30 19:10:38 -0300

    Upgrade tkn.

--- dev-tools/tkn.sh
+++ dev-tools/tkn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-export TKN_VERSION=0.25.0
+export TKN_VERSION=0.31.1
 
 curl -LO https://github.com/tektoncd/cli/releases/download/v${TKN_VERSION}/tkn_${TKN_VERSION}_Linux_x86_64.tar.gz
 sudo tar xvzf tkn_${TKN_VERSION}_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
