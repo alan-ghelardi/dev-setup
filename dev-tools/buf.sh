d922ddd6268e9b961da29943f677807459c3d8b3
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2024-08-23 14:39:20 -0300

    Install buf.

---
+++ dev-tools/buf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PREFIX="/usr/local" && \
+    VERSION="1.38.0" && \
+    curl -sSL \
+         "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m).tar.gz" | \
+        sudo tar -xvzf - -C "${PREFIX}" --strip-components 1
